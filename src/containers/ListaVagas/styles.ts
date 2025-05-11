a945408e7999a089e1f3f83ade5ff0443e7566c6
iury santos <iurysantos654@gmail.com>
2025-05-10 21:07:27 -0300

    correcão desnecessaria dnv...

--- src/containers/ListaVagas/styles.ts
+++ src/containers/ListaVagas/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const VagasContainer = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1 fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   column-gap: 16px;
   row-gap: 16px;
   margin-top: 32px;
